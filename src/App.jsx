e2a259e3bf021f54ca82524dc77a20193a15a809
DevKLS <email-que-voce-usou-no-github>
2026-05-04 21:21:25 -0300

    Correção dos imports e rotas (Ex01 até Ex16)

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import "./App.css";
 import Layout from "./componentes/Layout";
 import Home from "./pages/Home";
 
-import Ex1 from "./pages/Ex1";
-import Ex2 from "./pages/Ex2";
-import Ex3 from "./pages/Ex3";
-import Ex4 from "./pages/Ex4";
-import Ex5 from "./pages/Ex5";
-import Ex6 from "./pages/Ex6";
-import Ex7 from "./pages/Ex7";
-import Ex8 from "./pages/Ex8";
-import Ex9 from "./pages/Ex9";
+import Ex01 from "./pages/Ex01";
+import Ex02 from "./pages/Ex02";
+import Ex03 from "./pages/Ex03";
+import Ex04 from "./pages/Ex04";
+import Ex05 from "./pages/Ex05";
+import Ex06 from "./pages/Ex06";
+import Ex07 from "./pages/Ex07";
+import Ex08 from "./pages/Ex08";
+import Ex09 from "./pages/Ex09";
 import Ex10 from "./pages/Ex10";
 import Ex11 from "./pages/Ex11";
 import Ex12 from "./pages/Ex12";
@@ -29,22 +29,22 @@ function App() {
         <Route path="/" element={<Home />} />
 
         {/* EXERCÍCIOS COM LAYOUT */}
-        <Route path="/ex1" element={<Layout><Ex1 /></Layout>} />
-        <Route path="/ex2" element={<Layout><Ex2 /></Layout>} />
-        <Route path="/ex3" element={<Layout><Ex3 /></Layout>} />
-        <Route path="/ex4" element={<Layout><Ex4 /></Layout>} />
-        <Route path="/ex5" element={<Layout><Ex5 /></Layout>} />
-        <Route path="/ex6" element={<Layout><Ex6 /></Layout>} />
-        <Route path="/ex7" element={<Layout><Ex7 /></Layout>} />
-        <Route path="/ex8" element={<Layout><Ex8 /></Layout>} />
-        <Route path="/ex9" element={<Layout><Ex9 /></Layout>} />
-        <Route path="/ex10" element={<Layout><Ex10 /></Layout>} />
-        <Route path="/ex11" element={<Layout><Ex11 /></Layout>} />
-        <Route path="/ex12" element={<Layout><Ex12 /></Layout>} />
-        <Route path="/ex13" element={<Layout><Ex13 /></Layout>} />
-        <Route path="/ex14" element={<Layout><Ex14 /></Layout>} />
-        <Route path="/ex15" element={<Layout><Ex15 /></Layout>} />
-        <Route path="/ex16" element={<Layout><Ex16 /></Layout>} />
+     <Route path="/ex1" element={<Layout><Ex01 /></Layout>} />
+     <Route path="/ex2" element={<Layout><Ex02 /></Layout>} />
+     <Route path="/ex3" element={<Layout><Ex03 /></Layout>} />
+     <Route path="/ex4" element={<Layout><Ex04 /></Layout>} />
+     <Route path="/ex5" element={<Layout><Ex05 /></Layout>} />
+     <Route path="/ex6" element={<Layout><Ex06 /></Layout>} />
+     <Route path="/ex7" element={<Layout><Ex07 /></Layout>} />
+     <Route path="/ex8" element={<Layout><Ex08 /></Layout>} />
+     <Route path="/ex9" element={<Layout><Ex09 /></Layout>} />
+     <Route path="/ex10" element={<Layout><Ex10 /></Layout>} />
+     <Route path="/ex11" element={<Layout><Ex11 /></Layout>} />
+     <Route path="/ex12" element={<Layout><Ex12 /></Layout>} />
+     <Route path="/ex13" element={<Layout><Ex13 /></Layout>} />
+     <Route path="/ex14" element={<Layout><Ex14 /></Layout>} />
+     <Route path="/ex15" element={<Layout><Ex15 /></Layout>} />
+     <Route path="/ex16" element={<Layout><Ex16 /></Layout>} />
 
       </Routes>
     </BrowserRouter>
